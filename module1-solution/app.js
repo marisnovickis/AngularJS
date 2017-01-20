bd65dc713adbd7ba780e419ef90458a8e1656d69
Maris Novickis <Maris Novickis>
2017-01-20 22:38:08 +0000

    implemented code, ready for review

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,29 +7,29 @@ LunchCheckController.$inject = ['$scope', '$filter'];
 
 function LunchCheckController ($scope, $filter) {
   $scope.lunchMenu = "";
+  $scope.message = "";
 
-
-  $scope.displayMessage = function() {
+  $scope.calculateMessage = function() {
     if ($scope.getFoodCount()==0) {
-      return "Please enter data first";
+      $scope.message = "Please enter data first";
+    } else if ($scope.getFoodCount()<=3) {
+      $scope.message = "Enjoy!";
+    } else {
+      $scope.message = "Too much!";
     }
-
-    if ($scope.getFoodCount()<=3) {
-      return "Enjoy!";
-    }
-
-    return "Too much!";
   };
 
   $scope.getFoodCount = function() {
     var arr = $scope.lunchMenu.split(",");
     var count = 0;
     for (var i = 0; i < arr.length; i++) {
-      if (arr[i].length>0 &&  !(arr[i].replace(/^\s+/, '').replace(/\s+$/, '')==='') ){
+      if (arr[i].length>0 &&  !(arr[i].replace(/^\s+/, '').replace(/\s+$/, '')==='') ){ //Check for whitespace food entry
         count++;
       }
     }
     return count;
   };
+
+  $scope.calculateMessage(); // Run to initialize default value
 }
 })();
